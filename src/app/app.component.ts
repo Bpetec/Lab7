0a81338a60e7139c485658d13d04e265e3106295
bpetec <bpetec@yahoo.com>
2019-11-16 13:12:41 -0800

    adding ending code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ToastService } from './toast/toast.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toast-app';
+  toastTypes: Array<string> = [];
+// constructor manually added
+  constructor(private toastService: ToastService) {
+    this.toastTypes = ['success', 'info', 'warning', 'danger'];
+
+  }
+// success, info, warning, danger
+  showToast() {
+    const rand = Math.floor(Math.random() * 4);
+    console.log('Rand: ', + rand);
+    const toastType = this.toastTypes[rand];
+    const toastMessage = 'This is a message my random number is: ' + rand;
+    const duration = 4000;
+    this.toastService.showToast(toastType, toastMessage, duration);
+  }
 }
